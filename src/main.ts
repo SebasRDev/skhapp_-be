104462dbe1dba527fae082200441fbb48e4d8c3a
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2025-02-21 00:59:27 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
